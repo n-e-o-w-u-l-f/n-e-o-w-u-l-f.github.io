45c1741a96e4b3f49874efac26264e81e7b2be97
Hammerschlag <megalod0n@live.de>
2019-11-15 15:53:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+function explode(target, size, xpos, ypos) {
+    if (!size) size = 64;
+    if (!xpos) xpos = 0;
+    if (!ypos) ypos = 0;
+
+    var explosion = document.createElement("div");
+    explosion.className = "explosion";
+    explosion.style.width = size + "px";
+    explosion.style.height = size + "px";
+
+    var h = '<div class="explosion-inner"><div class="explosion-line-inner"></div></div>';
+
+    var x = '';
+    x += '<div class="explosion-inner">';
+
+    x += '<div class="explosion-line left" style="width:' + size + 'px;height:' + size + 'px">' + h + '</div>';
+    x += '<div class="explosion-line right" style="width:' + size + 'px;height:' + size + 'px">' + h + '</div>';
+    x += '<div class="explosion-line up" style="width:' + size + 'px;height:' + size + 'px">' + h + '</div>';
+    x += '<div class="explosion-line down" style="width:' + size + 'px;height:' + size + 'px">' + h + '</div>';
+    x += '<div class="explosion-line tr" style="width:' + size + 'px;height:' + size + 'px">' + h + '</div>';
+    x += '<div class="explosion-line dr" style="width:' + size + 'px;height:' + size + 'px">' + h + '</div>';
+    x += '<div class="explosion-line dl" style="width:' + size + 'px;height:' + size + 'px">' + h + '</div>';
+    x += '<div class="explosion-line ul" style="width:' + size + 'px;height:' + size + 'px">' + h + '</div>';
+
+    x += '</div>';
+    explosion.innerHTML = x;
+
+    target.appendChild(explosion);
+
+    setTimeout(function () {
+        if (!explosion) return;
+        explosion.parentNode.removeChild(explosion);
+    }, 750);
+
+    explosion.style.left = xpos + "px";
+    explosion.style.top = ypos + "px";
+
+    return explosion;
+}
+
+setInterval(function () {
+    var e = explode(document.body, 32, Math.random() * document.body.clientWidth, Math.random() * document.body.clientHeight);
+    e.style.zIndex = 0;
+}, 300);
+
+document.addEventListener('click', function (e) {
+    var blast = explode(document.body, 128, e.clientX, e.clientY);
+    blast.style.zIndex = 200;
+}, true);
